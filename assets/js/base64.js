e290dd045ee324db8940d2e1c1733f7f3cd489e5
noraworld <noraworld@mail.com>
2016-12-26 07:32:52 +0900

    :sparkles: Imprement base64 encode algorithm (halfway)

--- assets/js/base64.js
+++ assets/js/base64.js
@@ -1,5 +1,33 @@
 $(function() {
 
-  $('#result').val('aaa');
+  $('#encode').on('submit', function(event) {
+    event.preventDefault();
+    var result = encode($('#source').val());
+    $('#result').val(result);
+  });
+
+  $('#result').on('click', function() {
+    $(this).select();
+  });
+
+  $('#copy').on('click', function() {
+    $('#result').select();
+    document.execCommand('copy');
+  });
+
+  function encode(str) {
+    var result = '';
+    var num;
+    var r;
+    for (var i = 0; i < str.length; i++) {
+      num = str[i].charCodeAt().toString(2);
+      result += ('00000000' + num).slice(-8);
+    }
+    r = new RegExp('.{1,' + 6 + '}', 'g');
+    result = result.match(r);
+    result[result.length-1] = (result[result.length-1] + '000000').slice(6);
+    console.log(result);
+    return result;
+  }
 
 });
